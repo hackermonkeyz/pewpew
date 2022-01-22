df891f7f1f67e245923582d8a9a870b63265c0af
hackermonkeyz <josh.sitsu@protonmail.com>
2022-01-21 22:13:58 -0500

    bug fixes

--- pewpew.py
+++ pewpew.py
@@ -1,7 +1,7 @@
 """
 pewpew: selenium spraying script for all them crazy m0nkeys
 """
-import argparse,os,sys,random,time
+import argparse,random,time
 from argparse import RawTextHelpFormatter
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
