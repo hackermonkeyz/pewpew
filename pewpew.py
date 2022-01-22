ba5590605c4888c2b07f2003c03284eb5e0d0d1e
hackermonkeyz <josh.sitsu@protonmail.com>
2022-01-21 22:24:50 -0500

    bug fixes

--- pewpew.py
+++ pewpew.py
@@ -66,7 +66,7 @@ def automate_keyboard(url,proxy,usersfile,password,delay,pause,resolution,xpath_
         users = [line.rstrip() for line in f]
         for user in users:
             driver.get(url)
-
+            waitforit(delay)
 
 
 
