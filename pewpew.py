1b69702bebf5f56ad186a6a718413678b7b272da
hackermonkeyz <josh.sitsu@protonmail.com>
2022-01-21 21:58:01 -0500

    bug fixes

--- pewpew.py
+++ pewpew.py
@@ -9,13 +9,21 @@ from selenium.common.exceptions import NoSuchElementException
 
 def create_driver(proxyaddr, user, password, resolution):
         
+    proxy = {'address': proxyaddr,
+         'username': 'bubbles',
+         'password_token': 'bW9ua2V5Q1RGezh2ZnZ3ZjNrYXc0dnhjejQxZDc4dnp4cmx0djczY3cwfQ==',
+         }
+         
     capability = webdriver.DesiredCapabilities.CHROME
     capability['proxy'] = {
-        "httpProxy": proxyaddr,
-        "ftpProxy": proxyaddr,
-        "sslProxy": proxyaddr,
+        "httpProxy": proxy['address'],
+        "ftpProxy": proxy['address'],
+        "sslProxy": proxy['address'],
         "proxyType": "manual"
     }
+
+    capability['proxy']['socksUsername'] = proxy['username']
+    capability['proxy']['socksPassword'] = proxy['password']
 
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--incognito")
